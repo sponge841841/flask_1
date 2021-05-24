f9c46a354fe70dcc36c09fa2c20ebcac9ff9dff0
根本大地 <daichi@nemotodaichinoMacBook-ea.local>
2021-05-25 01:00:00 +0900

    initial commit

---
+++ main_2.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def hello_world():
+  return "hello_world"
+
+
+@app.route("/")
+def index():
+  return url_for("show_user_profile", username="HAIT_Lab")
+
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+  return "UserName: " + str(username)
+
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+  return "Post" + str(post_id)
+
+
+if __name__ == "__main__":
+  app.run(port=8000, debug=True)
